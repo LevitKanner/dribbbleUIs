0115cd3b5b0b11f4b5e374ba3f4655ad0fcb95ed
Levit Kanner <lkanner21@gmail.com>
2022-08-11 20:20:39 +0000

    Resolves conflict

--- src/App.js
+++ src/App.js
@@ -1,42 +1,10 @@
 import './App.css';
-import AppHeader from "./components/AppHeader";
-import Banner from "./components/Banner";
-import Note from "./components/Note";
+import {Builder_au} from "./views/Builder_au";
 
-const notes = [
-    {
-        number: "01",
-        text: "Property in united",
-        city: "States"
-    },
-    {
-        number: "02",
-        text: "Commercial Space in",
-        city: "Canada"
-    },
-    {
-        number: "03",
-        text: "Plots in High Tower",
-        city: "in LA"
-    },
-]
 
 function App() {
     return (
-        <div className="bg-gradient-to-t from-[#97FFFF] to-[#56D3F4] min-h-screen">
-            <main className="max-w-7xl mx-auto ">
-                <AppHeader/>
-                <div className="px-6 xl:px-8">
-                    <Banner/>
-                    <section className="flex flex-col space-y-8 md:flex-row md:space-x-8 md:space-y-0 mt-16 xl:mt-32">
-                        {notes.map(note => <Note city={note.city} number={note.number} text={note.text}
-                                                 key={note.number}/>)}
-                    </section>
-                </div>
-            </main>
-        </div>
-
-
+        <Builder_au />
     );
 }
 
---
+++ src/views/Builder_au.js
@@ -0,0 +1,36 @@
+import AppHeader from "../components/AppHeader";
+import Banner from "../components/Banner";
+import Note from "../components/Note";
+
+export function Builder_au() {
+    const notes = [
+        {
+            number: "01",
+            text: "Property in united",
+            city: "States"
+        },
+        {
+            number: "02",
+            text: "Commercial Space in",
+            city: "Canada"
+        },
+        {
+            number: "03",
+            text: "Plots in High Tower",
+            city: "in LA"
+        },
+    ]
+    return <div className="bg-gradient-to-t from-[#97FFFF] to-[#56D3F4] min-h-screen">
+        <main className="max-w-7xl mx-auto ">
+            <AppHeader/>
+            <div className="px-6">
+                <Banner/>
+                <section className="flex space-x-8 mt-16">
+                    {notes.map(note =>
+                        <Note city={note.city} number={note.number} text={note.text}
+                              key={note.number}/>)}
+                </section>
+            </div>
+        </main>
+    </div>;
+}
