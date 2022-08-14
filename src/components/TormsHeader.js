0d11d2c289a007e81e542fee3baa1b4e22c3d5de
Levit Kanner <lkanner21@gmail.com>
2022-08-14 19:02:34 +0000

    Add hover state to header links

--- src/components/TormsHeader.js
+++ src/components/TormsHeader.js
@@ -1,10 +1,10 @@
 import {BiSearchAlt2} from "react-icons/bi";
 import {BsBell} from "react-icons/bs";
 
-const TormsHeader = (props) => {
+const TormsHeader = () => {
     return (
         <>
-            <div className="px-10 pt-3 bg-gray-100">
+            <div className="px-10 pt-3 bg-gray-100 flex-shrink-0 w-[92vw]">
                 <div className="flex justify-between items-center">
                     <span className="self-end font-bold"> Dashboard</span>
                     <div
@@ -16,10 +16,10 @@ const TormsHeader = (props) => {
                 </div>
                 <nav className="mt-3">
                     <ul className="flex space-x-6 items-center text-sm">
-                        <li className="py-2 border-b-2 border-black font-bold"> Summary</li>
-                        <li className="py-2 text-gray-400"> Statistics</li>
-                        <li className="py-2 text-gray-400"> Overview</li>
-                        <li className="py-2 text-gray-400"> Account</li>
+                        <li className="py-2 text-gray-400 hover:text-black hover:border-b-2 hover:border-black hover:font-bold transition-all ease-linear"> Summary</li>
+                        <li className="py-2 text-gray-400 hover:text-black hover:border-b-2 hover:border-black hover:font-bold transition-all ease-linear"> Statistics</li>
+                        <li className="py-2 text-gray-400 hover:text-black hover:border-b-2 hover:border-black hover:font-bold transition-all ease-linear"> Overview</li>
+                        <li className="py-2 text-gray-400 hover:text-black hover:border-b-2 hover:border-black hover:font-bold transition-all ease-linear"> Account</li>
                     </ul>
                 </nav>
                 <div>
