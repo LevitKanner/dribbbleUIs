4eb3f65e572e88c907c7cb68419f005173f9509e
Levit Kanner <lkanner21@gmail.com>
2022-08-16 20:29:05 +0000

    Fix torm header width

--- src/components/TormsHeader.js
+++ src/components/TormsHeader.js
@@ -8,7 +8,7 @@ const TormsHeader = () => {
                 <div className="flex justify-between items-center">
                     <span className="self-end font-bold"> Dashboard</span>
                     <div
-                        className="flex space-x-6 items-center w-[45%] justify-end border-b border-black py-1.5">
+                        className="flex space-x-6 items-center w-[44%] justify-end border-b border-black py-1.5">
                         <BiSearchAlt2 className="w-4 h-4"/>
                         <BsBell className="w-4 h-4"/>
                         <div className="w-4 h-4 bg-gray-300"></div>
