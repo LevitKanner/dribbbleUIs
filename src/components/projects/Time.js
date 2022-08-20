4ba6fa6a55c7e63895d38c5c7ac8fa868945f6b1
Levit Kanner <lkanner21@gmail.com>
2022-08-20 20:56:26 +0000

    Add time component

--- src/components/projects/Time.js
+++ src/components/projects/Time.js
@@ -1,15 +1,18 @@
 import {MdKeyboardArrowDown} from "react-icons/md";
 
-const TeamStatus = () => {
+const Time = () => {
     return (
-        <div className="w-[48%] h-full mt-4 rounded p-5 bg-slate-800">
+        <div className="w-[49%] h-full mt-4 rounded-md p-5 bg-slate-800">
             <div className="flex items-center justify-between text-white">
                 <span> Time </span>
-                <span className="flex items-center text-sm p-1 rounded border border-white/20"> <span className="px-0.5">This week</span> <MdKeyboardArrowDown/>  </span>
+                <span className="flex items-center text-sm p-1 rounded border border-white/20">
+                    <span className="px-0.5">This week</span>
+                    <MdKeyboardArrowDown/>
+                </span>
             </div>
 
         </div>
     )
 };
 
-export default TeamStatus;
+export default Time;
