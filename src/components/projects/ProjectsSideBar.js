64b80514f5ba1f49622949aa80a89e48a4a3ea43
Levit Kanner <lkanner21@gmail.com>
2022-08-21 15:17:10 +0000

    Modify hover state of sidebar

--- src/components/projects/ProjectsSideBar.js
+++ src/components/projects/ProjectsSideBar.js
@@ -11,24 +11,24 @@ const ProjectsSideBar = () => {
             <span className="w-10 h-10 rounded-full bg-indigo-900"></span>
             <nav className="w-full">
                 <ul className="flex flex-col items-center space-y-4 w-full">
-                    <li className="w-full flex justify-center hover:text-green-600 py-4 hover:border-r-4 hover:border-indigo-600 hover:bg-indigo-600/20 transition-all ease-linear">
-                        <BsSpeedometer className="w-5 h-5 text-white"/>
+                    <li className="group w-full flex justify-center py-4 hover:border-r-4 hover:border-indigo-600 hover:bg-indigo-600/20 transition-all ease-linear">
+                        <BsSpeedometer className="w-5 h-5 text-white/40 group-hover:text-white"/>
                     </li>
-                    <li className="w-full flex justify-center hover:text-green-600 py-4  hover:border-r-4 hover:border-indigo-600 hover:bg-indigo-600/20  transition-all ease-linear">
-                        <MdOutlineFolderOpen className="w-5 h-5 text-white"/>
+                    <li className="group w-full flex justify-center py-4  hover:border-r-4 hover:border-indigo-600 hover:bg-indigo-600/20  transition-all ease-linear">
+                        <MdOutlineFolderOpen className="w-5 h-5 text-white/40 group-hover:text-white"/>
                     </li>
-                    <li className="w-full flex justify-center hover:text-green-600 py-4  hover:border-r-4 hover:border-indigo-600 hover:bg-indigo-600/20  transition-all ease-linear">
-                        <FiCalendar className="w-5 h-5 text-white"/>
+                    <li className="group w-full flex justify-center py-4  hover:border-r-4 hover:border-indigo-600 hover:bg-indigo-600/20  transition-all ease-linear">
+                        <FiCalendar className="w-5 h-5 text-white/40 group-hover:text-white"/>
                     </li>
-                    <li className="w-full flex justify-center hover:text-green-600 py-4  hover:border-r-4 hover:border-indigo-600 hover:bg-indigo-600/20  transition-all ease-linear">
-                        <RiInboxLine className="w-5 h-5 text-white"/>
+                    <li className="group w-full flex justify-center py-4  hover:border-r-4 hover:border-indigo-600 hover:bg-indigo-600/20  transition-all ease-linear">
+                        <RiInboxLine className="w-5 h-5 text-white/40 group-hover:text-white"/>
                     </li>
-                    <li className="w-full flex justify-center hover:text-green-600 py-4  hover:border-r-4 hover:border-indigo-600 hover:bg-indigo-600/20  transition-all ease-linear">
-                        <RiPieChart2Line className="w-5 h-5 text-white"/>
+                    <li className="group w-full flex justify-center py-4  hover:border-r-4 hover:border-indigo-600 hover:bg-indigo-600/20  transition-all ease-linear">
+                        <RiPieChart2Line className="w-5 h-5 text-white/40 group-hover:text-white"/>
                     </li>
                 </ul>
             </nav>
-            <span> <TbSettings className="w-5 h-5 text-white"/> </span>
+            <span> <TbSettings className="w-5 h-5 text-white/40"/> </span>
         </aside>
     )
 };
